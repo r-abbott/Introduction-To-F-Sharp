52da87c1576055be0fd72f6c7d24ad07866ae44f
r-abbott <r-abbott@users.noreply.github.com>
2017-03-03 14:18:51 -0600

    Added Precomputation example

---
+++ Precomputation/Program.fs
@@ -0,0 +1,28 @@
+﻿open System.Collections.Generic
+
+let isInList (list: 'a list) v =
+    let lookupTable = new HashSet<'a>(list)
+    printfn "Lookup table created, looking up value"
+    lookupTable.Contains v
+
+printfn "%b" (isInList["hi";"there";"oliver"] "there")
+printfn "%b" (isInList["hi";"there";"oliver"] "anna")
+
+// trying to create the list only once, but this does not work
+// the list gets created each call
+let isInListClever = isInList["hi";"there";"oliver"]
+printfn "%b" (isInListClever "there")
+printfn "%b" (isInListClever "anna")
+
+// Instead, you can return a function for the lookup step
+// leaving the construction of the list step to happen only once
+let constructLookup (list: 'a list) =
+    let lookupTable = new HashSet<'a>(list)
+    printfn "Lookup table created"
+    fun v ->
+        printfn "Looking up value"
+        lookupTable.Contains v
+
+let isInListClever' = constructLookup ["hi";"there";"oliver"]
+printfn "%b" (isInListClever' "there")
+printfn "%b" (isInListClever' "anna")
