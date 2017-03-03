343242e5df952003a446e4f78cdf25e1163e8e38
r-abbott <r-abbott@users.noreply.github.com>
2017-03-03 15:49:37 -0600

    Tuples examples

---
+++ Tuples/Program.fs
@@ -0,0 +1,15 @@
+﻿let t1 = 12, 5, 7
+let v1, v2, v3 = t1
+
+let t2 = "hi", true
+
+printfn "%A %A" (fst t2) (snd t2)
+
+let third t =
+    let _, _, r = t
+    r
+printfn "%A" (third t1)
+
+// Pattern matching
+let third' (_,_,r) = r
+printfn "%A" (third' t1)
