4ab9d3bc56b6b1584c8d0bdc3628f753021c5e30
r-abbott <r-abbott@users.noreply.github.com>
2017-03-03 14:26:41 -0600

    Added recusion examples

---
+++ Recursion/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+open System.Threading
+
+// recursive function needs to be declared as "rec"
+let rec fact x =
+    if x = 1 then 1
+    else x * (fact (x - 1))
+
+printfn "%d" (fact 5)
+
+// Recursive call between functions needs the "and" instruction
+let rec fnA() = fnB()
+and fnB() = fnA()
+
+// recursively display random number every second, rather than using
+// a while loop
+let showValues() =
+    let r = Random()
+    let rec dl() =
+        printfn "%d" (r.Next())
+        Thread.Sleep(1000)
+        dl()
+
+    dl()
+
+showValues()
