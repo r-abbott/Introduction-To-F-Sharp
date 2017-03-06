ae907681b1dc4446c1618d71ff1c8621f8e2a70a
r-abbott <r-abbott@users.noreply.github.com>
2017-03-06 11:24:23 -0600

    Added Exceptions and IDisposable examples

---
+++ ExceptionsAndDisposable/Program.fs
@@ -0,0 +1,73 @@
+﻿// Exceptions
+
+// Declaring an exception
+exception MyCustomException of int * string
+    with
+        override x.Message =
+            let (MyCustomException(i,s)) = upcast x
+            sprintf "Int: %d Str: %s" i s
+
+// Raise an exception
+//raise (MyCustomException(10, "Error!"))
+
+// Fail with message
+//failwith "Some error occurred"
+
+let rec fact x =
+    if x < 0 then invalidArg "x" "Value must be >= 0"
+    match x with
+    | 0 -> 1
+    | _ -> x * (fact (x - 1))
+
+let output (o: obj) =
+    try
+        let os = o :?> string
+        printfn "Object is %s" os
+    with
+    | :? System.InvalidCastException as ex -> printfn "Can't cast, message was: %s" ex.Message
+
+//output 35
+
+// try with as expression
+let result =
+    try
+        Some(10 / 0)
+    with
+    | :? System.DivideByZeroException ->  None
+
+printfn "%A" result
+
+try
+    raise (MyCustomException(3, "text"))
+with
+| MyCustomException(i, s) -> printfn "Caught custom exception with %d, %s" i s
+
+let getValue() =
+    try
+        printfn "Returning Value"
+        42
+    finally
+        printfn "In the finally block now"
+
+getValue()
+
+// IDisposable
+
+let createDisposable f=
+    { new System.IDisposable with member x.Dispose() = f()}
+
+let outerFunction() =
+    use disposable = createDisposable (fun() -> printfn "Now disposing myself")
+    printfn "In outer function"
+
+// Won't call Dispose when using "let disposable = ..."
+// Will call Dispose when using "use disposable = ..." instead of let
+outerFunction()
+
+// using keyword example
+let outerFunction'() =
+    using (createDisposable( fun() -> printfn "Now disposing myself"))
+        (fun d -> printfn "Acting on the disposable object now")
+    printfn "In outer function"
+
+outerFunction'()
---
+++ InterfacesAndObjectExpressions/Program.fs
@@ -0,0 +1,59 @@
+﻿// Interface
+type IMyInterface =
+    abstract member Value: int with get
+
+type IDerivedInterface =
+    inherit IMyInterface
+
+    abstract member Add: int -> int -> int
+
+type MyClass() =
+    interface IMyInterface with
+        member x.Value with get() = 13
+
+type MyOtherClass() =
+    member this.Add x y = x + y
+
+    interface IDerivedInterface with
+        member i.Add x y = i.Add x y
+        member x.Value = 42
+
+let moc = MyOtherClass()
+
+printfn "%A" (moc.Add 10 20)
+// Have to upcast :> in order to use interface properties & methods
+printfn "%A" ((moc :> IMyInterface).Value)
+printfn "%A" ((moc :> IDerivedInterface).Add 12 30)
+
+// Object Expressions
+let hiObject = { new obj() with member x.ToString() = "Hi!" }
+
+printfn "%A" hiObject
+
+type IDeepThought =
+    abstract member TheAnswer: int with get
+    abstract member AnswerString: unit -> string
+
+type DeepThought() =
+    interface IDeepThought with
+        member x.TheAnswer = 42
+        member x.AnswerString() = sprintf "The answer is %d" (x :> IDeepThought).TheAnswer
+
+let htmlDeepThought =
+    let deepThought = DeepThought() :> IDeepThought
+    // Creating the implementation of IDeepThought
+    { new IDeepThought with
+        member x.TheAnswer = deepThought.TheAnswer
+        member x.AnswerString() = sprintf "<b>%s</b>" (deepThought.AnswerString())
+    }
+
+printfn "%A" (htmlDeepThought.AnswerString())
+    
+let confusedDeepThought answer =
+    { new IDeepThought with
+        member x.TheAnswer = answer
+        member x.AnswerString() = "uh..." }
+
+let cdt = confusedDeepThought 35
+printfn "%A" cdt.TheAnswer
+printfn "%A" (cdt.AnswerString())
